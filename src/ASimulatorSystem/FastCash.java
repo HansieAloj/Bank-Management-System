b24debd28f49e61a27c943f3fb76d0adf1313c89
Nijo Ninan <nijoninan23@gmail.com>
2021-07-11 01:16:56 +0530

    Changes in sql

--- src/ASimulatorSystem/FastCash.java
+++ src/ASimulatorSystem/FastCash.java
@@ -75,18 +75,32 @@ public class FastCash extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
+
         try {
             String amount = ((JButton)ae.getSource()).getText().substring(3); //k
             Conn c = new Conn();
-            ResultSet rs = c.s.executeQuery("select * from bank where pin = '"+pin+"'");
-            int balance = 0;
-            while (rs.next()) {
-                if (rs.getString("mode").equals("Deposit")) {
-                    balance += Integer.parseInt(rs.getString("amount"));
-                } else {
-                    balance -= Integer.parseInt(rs.getString("amount"));
-                }
-            } String num = "17";
+            // ResultSet rs = c.s.executeQuery("select * from bank where pin = '"+pin+"'");
+            // int balance = 0;
+            // while (rs.next()) {
+            //     if (rs.getString("mode").equals("Deposit")) {
+            //         balance += Integer.parseInt(rs.getString("amount"));
+            //     } else {
+            //         balance -= Integer.parseInt(rs.getString("amount"));
+            //     }
+
+
+
+            // } String num = "17";
+
+
+            ResultSet rs = c1.s.executeQuery("select balance from login where pin = '"+pin+"'");
+            if(rs.next())
+            {
+                balance = Integer.parseInt(rs.getString("balance"));
+            }
+
+
+
             if (ae.getSource() != b7 && balance < Integer.parseInt(amount)) {
                 JOptionPane.showMessageDialog(null, "Insuffient Balance");
                 return;
@@ -98,6 +112,8 @@ public class FastCash extends JFrame implements ActionListener {
             }else{
                 Date date = new Date();
                 c.s.executeUpdate("insert into bank values('"+pin+"', '"+date+"', 'Withdrawl', '"+amount+"')");
+                c1.s.executeUpdate("update login set balance = balance - "+amount+" where pin = '"+pin+"'");
+                
                 JOptionPane.showMessageDialog(null, "Rs. "+amount+" Debited Successfully");
                     
                 setVisible(false);
